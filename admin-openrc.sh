d1728bf0607a880c568644834fa79098e8c9223d
lihaitao <lihato@163.com>
2014-05-20 09:56:28 +0800

    Create admin-openrc.sh

---
+++ admin-openrc.sh
@@ -0,0 +1,4 @@
+export OS_USERNAME=admin                                                                                                                                                        
+export OS_PASSWORD=admin_pass
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=http://ctrl.openstack.com:35357/v2.0/
